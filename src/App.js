ac3e036aa4710bcc80936af32fe8f586bcaf47d0
OlgaKruglik <137288699+OlgaKruglik@users.noreply.github.com>
2025-02-03 19:06:16 +0300

    Fix: router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Registration from './Pages/Registration'; 
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import Registration from './Pages/Registration'
 import Home from './Pages/Home'
 
 
-const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/registration", element: <Registration /> },
-], {
-  future: {
-    v7_startTransition: true, 
-    v7_relativeSplatPath: true, 
-  },
-});
 
 function App() {
   return (
-    <div className="App">
-      <RouterProvider router={router} />
-    </div>
+    <Router basename="/OlgaKruglik/react-course-project.git">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
+    </Router>
   );
-}
+};
+
 
 export default App;
